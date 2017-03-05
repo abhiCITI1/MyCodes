e5e5d5c1c5c46b933bc267000b5a615197aa5e13
Abhishek Upadhyay <abhishek.up25@gmail.com>
2017-03-05 00:15:28 -0800

    Update socket-mon.py

--- socket-mon.py
+++ socket-mon.py
@@ -1,3 +1,11 @@
+/**
+Use psutil and implement a network socket monitoring tool that can check how many TCP sockets are being created by a web application.
+List all processes that have any socket connections (meaning the laddr and raddr fields exist).
+Group by the PID and sort the output by the number of the connections per process.
+**/
+
+
+
 import itertools
 import collections
 from itertools import groupby
